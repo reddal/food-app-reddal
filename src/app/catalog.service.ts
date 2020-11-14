3930dab276ac0ccbc0586c5f08ce21d2cd4ab205
Mariano Arias <mariano.arias12@gmail.com>
2020-11-14 01:45:17 -0300

    hosted images and changed its references for them to work on stackblitz

--- src/app/catalog.service.ts
+++ src/app/catalog.service.ts
@@ -16,7 +16,7 @@ export class CatalogService {
         description: "deliciosa porcion de brownie de chocolate con merengue",
         stock: 5,
         quantity: 0,
-        img_src: "assets/img/charlie_brownie.jpg",
+        img_src: "https://i.ibb.co/ZfSzpF3/charlie-brownie.jpg",
       },
       {
         id: 2,
@@ -26,7 +26,7 @@ export class CatalogService {
         description: "3 donas con glaseado rosa y chispitas de colores",
         stock: 0,
         quantity: 0,
-        img_src: "assets/img/homer_donut.jpg",
+        img_src: "https://i.ibb.co/CKz4G0k/homer-donut.jpg",
       },
       {
         id: 3,
@@ -36,17 +36,17 @@ export class CatalogService {
         description: "Es una porcion lemon pie, no hay mucho vuelta",
         stock: 10,
         quantity: 0,
-        img_src: "assets/img/john_lemon.jpg",
+        img_src: "https://i.ibb.co/NNxmTwW/john-lemon.jpg",
       },
       {
         id: 4,
         name: "Tangerine dream",
         type: "Budin de mandarinas",
         price: 150,
-        description: "mermeladamermeladamermeladamermeladamermelada",
+        description: "exquisito budin vegano de mandarinas y chía",
         stock: 2,
         quantity: 0,
-        img_src: "assets/img/tangerine_dream.jpeg",
+        img_src: "https://i.ibb.co/v1h2Q7C/tangerine-dream.jpg",
       },
     ];
   catalog: BehaviorSubject<Sweet[]> = new BehaviorSubject([]);
